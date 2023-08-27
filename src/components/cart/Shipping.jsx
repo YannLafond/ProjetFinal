15ee5f7608ed7108ae83ad7db65407bf2b709d76
Yann Lafond <yann.lafond.ly@gmail.com>
2023-08-27 23:49:00 +0200

    Ajout du site statique

---
+++ src/components/cart/Shipping.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Country, State } from "country-state-city";
+import Popup from 'reactjs-popup';
+import { Link } from "react-router-dom";
+
+const Shipping = () => {
+  return (
+    <section className="shipping">
+      <main>
+        <h1>Détail de la livraison</h1>
+        <form>
+          <div>
+            <label>Adresse</label>
+            <input type="text" placeholder="Introduisez votre adresse" />
+          </div>
+          <div>
+            <label>Ville</label>
+            <input type="text" placeholder="Introduisze votre ville" />
+          </div>
+          <div>
+              {/* Compelte the code for the COUNTRY DROPDOWN*/}
+            <label>Pays</label>
+
+            <select>
+              <option value="">Pays</option>
+              // Enter the code here for country dropdown
+              {Country && Country.getAllCountries().map((i) => ( 
+              <option value="{i.isoCode}" key="{i.isoCode}">
+                {i.name}
+              </option>
+              ))}                
+            </select>
+          </div>
+          
+          <div>
+              {/* Add the code for the STATE DROPDOWN*/}
+              <label>Etat/Région</label>
+
+              <select>
+                <option value="">Etat/Région</option>                
+                {State && State.getStatesOfCountry("IN").map((i) =>( 
+                <option value="{i.isoCode}" key="{i.isoCode}">
+                  {i.name}
+                </option>
+                ))}                
+              </select>
+          </div>
+
+          <div>
+            <label>Code postal</label>
+            <input type="number" placeholder="Introduisez votre code postal" />
+          </div>
+                  
+          <div>
+            <label>N° de téléphone</label>
+            <input type="number" placeholder="Introduisez votre n° de téléphone" />
+          </div>
+          <Popup trigger=
+                {<Link className="link" to="/myorders">Confirmer la commande</Link>}
+                position="right center">
+                <div style={{color:"red",position: 'absolute', top: '50%', right: '100%', transform: 'translateY(-50%)', 
+                      backgroundColor: '#fff', padding: '10px', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)'}}>
+                      Votre commande est encours de traitement !</div>              
+          </Popup>
+        </form>
+      </main>
+    </section>
+  );
+};
+
+export default Shipping;
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react"; 
+import { IoFastFoodOutline } from "react-icons/io5"; 
+import { Link } from "react-router-dom"; 
+import { FiShoppingCart, FiLogIn } from "react-icons/fi"; 
+import { FaUsers } from "react-icons/fa";
+import { motion } from "framer-motion"; 
+import DropdownMenu from './DropdownMenu';
+
+
+const Header = ({ isAuthenticated = false }) => {
+    return (
+        <nav>
+            <motion.div initial={{ x: "-100%" }} whileInView={{ x: 0 }}>
+                <IoFastFoodOutline />
+            </motion.div>
+
+            <div>
+            <Link to="/">Accueil</Link> 
+            <Link to="/contact">Nous contacter</Link> 
+            <Link to="/about">A propos</Link> 
+            <Link to="/cart"> 
+                <FiShoppingCart /> 
+            </Link>
+
+            <DropdownMenu />
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
