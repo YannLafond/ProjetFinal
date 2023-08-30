73650dd46e618530e9eda52579edde75c34195f4
Yann Lafond <yann.lafond.ly@gmail.com>
2023-08-30 06:48:12 +0200

    modification de data et page menu rendu dynamique

--- src/components/home/Menu.jsx
+++ src/components/home/Menu.jsx
@@ -1,8 +1,6 @@
 import React from "react"; 
 import MenuCard from "./MenuCard"; 
-import burger1 from "../../assets/burger1.png"; 
-import burger2 from "../../assets/burger2.png"; 
-import burger3 from "../../assets/burger3.png";
+import data from "../../data"
 
 
 const Menu = () => { 
@@ -10,33 +8,22 @@ const Menu = () => {
     
     return ( 
         <section id="menu"> 
-            <h1>MENU</h1> 
-    
-            <div> 
-                <MenuCard 
-                    itemNum={1} 
-                    burgerSrc={burger1} 
-                    price={200} 
-                    title="Cheese Burger" 
-                    handler={addToCartHandler} 
-                    delay={0.1} 
-                /> 
-                <MenuCard 
-                    itemNum={2} 
-                    burgerSrc={burger2}
-                    price={500} 
-                    title="Cheese Burger vegane" 
-                    handler={addToCartHandler}
-                    delay={0.5}  
-                /> 
-                <MenuCard 
-                    itemNum={3} 
-                    burgerSrc={burger3}
-                    price={1800} 
-                    title="Cheese Burger avec frites"  
-                    handler={addToCartHandler}
-                    delay={0.8} 
-                /> 
+            <h1>MENU</h1>    
+            <div>
+                {data.productData.map((item, index) => {
+                    return(
+                        <MenuCard 
+                            itemNum={item.id} 
+                            burgerSrc={item.img} 
+                            price={item.price} 
+                            title={item.title}
+                            handler={addToCartHandler} 
+                            delay={item.delay} 
+                        />
+                    )
+                })}
+                
+            
             </div> 
         </section> 
     ); 
--- src/data.js
+++ src/data.js
@@ -1,25 +1,35 @@
+import img1 from "../src/assets/burger1.png"
+import img2 from "../src/assets/burger2.png"
+import img3 from "../src/assets/burger3.png"
+
 const data = {
     productData:[
         {
             id: 1,
-            img: "./assets/burger1.png",
+            img: img1,
             title: "Cheese Burger",
             price: 200,
+            delay: 0.1,
         },
         {
             id: 2,
-            img: "./assets/burger2.png",
+            img: img2,
             title: "Cheese Burger Vegan",
             price: 500,
+            delay: 0.5,
+            
         },
         {
             id: 3,
-            img: "./assets/burger3.png",
+            img: img3,
             title: "Cheese Burger avec Frites",
             price: 1800,
+            delay: 0.8,
         }
     ]
 
 };
 
 export default data;
+
+
